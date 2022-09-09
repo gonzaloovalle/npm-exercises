a7400fb2725e7bd5949894be3d5c4841fd0db4a0
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-09-09 15:12:21 -0500

    added jquery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const $ = require('jquery')
+
 const helloWorld = () => console.log("Hello World");
 
 helloWorld();
+
+$('#codeup').click(function() {
+    alert('h2 with id "codeup" was clicked');
+});
+
+$('#codeup').hover(
+    function() {
+    $(this).css('background-color', "#FF0");
+    },
+    function() {
+    $(this).css('background-color', "#FFF");
+    }
+);
